8e059b96f720aeaab78f2bd0e19b95f9e686d3db
sang <sang@miro.com>
2023-04-25 22:34:13 +0200

    remove format of tcp packet

--- tcp/connection.go
+++ tcp/connection.go
@@ -1,14 +1,9 @@
 package tcp
 
 import (
-	"encoding/binary"
-	"fmt"
+	"io"
 	"net"
 )
-
-const packetHeaderSize = 4
-
-var maxPacketSize = 1024 * 1024
 
 // Connection data structure
 type Connection struct {
@@ -32,72 +27,17 @@ func Dial(addr string) (*Connection, error) {
 	}, nil
 }
 
-// Set max packet size (default: 1024 * 1024)
-func SetMaxPacketSize(size int) {
-	maxPacketSize = size
+// Return io.Reader interface
+func (c *Connection) Reader() io.Reader {
+	return c.socket
 }
 
-// Receive data from socket
-func (c *Connection) Receive() ([]byte, error) {
-	var data []byte
-	length, err := c.readPacketSize()
-	if err == nil {
-		readed := 0
-		bytes := 0
-		data = make([]byte, length)
-		for readed < length && err == nil {
-			bytes, err = c.socket.Read(data[readed:])
-			readed += bytes
-		}
-	}
-	return data, err
-}
-
-// Send data to socket
-func (c *Connection) Send(data []byte) error {
-	length := len(data)
-	err := c.writePacketSize(length)
-	if err == nil {
-		writed := 0
-		bytes := 0
-		for writed < length && err == nil {
-			bytes, err = c.socket.Write(data[writed:])
-			writed += bytes
-		}
-	}
-	return err
+// Return io.Writer interface
+func (c *Connection) Writer() io.Writer {
+	return c.socket
 }
 
 // Close connection
 func (c *Connection) Close() {
 	c.socket.Close()
-}
-
-func (c *Connection) readPacketSize() (int, error) {
-	header := make([]byte, packetHeaderSize)
-	bytesRead, err := c.socket.Read(header)
-	if err != nil || bytesRead != packetHeaderSize {
-		return 0, fmt.Errorf("read socket error from %s", c.socket.RemoteAddr())
-	}
-	length := int(binary.LittleEndian.Uint32(header))
-	if length == 0 {
-		return 0, fmt.Errorf("empty packet from %s", c.socket.RemoteAddr())
-	}
-	if length > maxPacketSize {
-		return 0, fmt.Errorf("data overflow from %s", c.socket.RemoteAddr())
-	}
-	return length, nil
-}
-
-func (c *Connection) writePacketSize(length int) error {
-	if length > maxPacketSize {
-		return fmt.Errorf("data overflow write to %s", c.socket.RemoteAddr())
-	}
-	header := make([]byte, packetHeaderSize)
-	binary.LittleEndian.PutUint32(header, uint32(length))
-	bytesWrite, err := c.socket.Write(header)
-	if err != nil || bytesWrite != packetHeaderSize {
-		return fmt.Errorf("write data error to %s", c.socket.RemoteAddr())
-	}
-	return nil
 }
